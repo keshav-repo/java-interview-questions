d1c2d3cb9ec368a73d871d97ef6f8e2b8a57e8a7
keshav <keshav.k987@gmail.com>
2023-04-26 12:55:19 +0530

    Custom Serialisation

--- src/main/java/core/serialization/Account.java
+++ src/main/java/core/serialization/Account.java
@@ -1,10 +1,38 @@
 package core.serialization;
 
+import java.io.IOException;
+import java.io.ObjectStreamException;
 import java.io.Serializable;
 
 public class Account implements Serializable {
+    private String username;
+    private transient String password;
 
+    public Account(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 
+    @Override
+    public String toString() {
+        return String.format("username: %s, password: %s", username, password);
+    }
 
+    private void readObject(java.io.ObjectInputStream stream) throws IOException, ClassNotFoundException {
+        System.out.println("Custom deserialization");
+        stream.defaultReadObject();
+        String encryptedPassword = (String) stream.readObject();
+        password = encryptedPassword.substring(3);
+    }
 
+    private void writeObject(java.io.ObjectOutputStream stream) throws IOException {
+        System.out.println("Custom serialization");
+        stream.defaultWriteObject();
+        String encryptedPassword = "enc".concat(password);
+        stream.writeObject(encryptedPassword);
+    }
+
+    private void readObjectNoData() throws ObjectStreamException {
+        System.out.println("custom readObjectNoData");
+    }
 }
--- src/main/java/core/serialization/CustomSerialisation.java
+++ src/main/java/core/serialization/CustomSerialisation.java
@@ -1,10 +1,19 @@
 package core.serialization;
 
+import java.io.*;
+
 public class CustomSerialisation {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Account account = new Account("user 1", "password");
 
+        FileOutputStream fos = new FileOutputStream("abc.ser");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(account);
 
+        FileInputStream fis = new FileInputStream("abc.ser");
+        ObjectInputStream ois = new ObjectInputStream(fis);
 
-    public static void main(String[] args) {
-
+        Account recovered = (Account) ois.readObject();
+        System.out.println(recovered.toString());
     }
 }
